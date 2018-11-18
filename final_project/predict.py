3089eba5f40948118e5746d35b9faca760487826
Dylan Larrabee <dylanlarrabee6@gmail.com>
2018-11-17 21:36:50 -0800

    first pass at train and predict scripts

---
+++ final_project/predict.py
@@ -0,0 +1,119 @@
+import torch
+import numpy as np
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+from collections import OrderedDict
+import argparse
+import json
+from PIL import Image
+from train import build_network
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--checkpoint', help="path to checkpoint", default='./checkpoint.pt')
+parser.add_argument('-', '--image', help="image path", default='/85/image_04805.jpg')
+parser.add_argument('-l', '--lables', help="JSON category mappings", default=None)
+parser.add_argument('-g', '--gpu', default=True)
+args = parser.parse_args()
+
+if args.gpu == True:
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+else:
+    device = torch.device("cpu")
+
+def load_model(path): # 'checkpoint.pt'
+    checkpoint = torch.load(path, map_location={'cuda:0': 'cpu'})
+
+    arch = checkpoint['arch']
+    out_features = len(checkpoint['class_to_idx'])
+    hidden_layers = checkpoint['hidden_layers']
+
+    # For loading, use the same build_network function used to make saved model's network
+    model = build_network(arch, out_features, hidden_layers)
+    model.load_state_dict(checkpoint['state_dict'])
+
+    model.class_to_idx = checkpoint['class_to_idx']
+    
+    return model
+    
+loaded_model = load_model(args.checkpoint)
+print("loaded checkpoint")
+
+def process_image(image_path):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    # Define same transforms that are used in training images
+    image_loader = transforms.Compose([transforms.Resize(255),
+                                     transforms.CenterCrop(224),
+                                     transforms.ToTensor()])
+    # open the image
+    pil_image = Image.open(image_path)
+    # transform the image
+    pil_image = image_loader(pil_image)
+    # Turn into np_array
+    np_image = np.array(pil_image)
+    
+    # undo mean and std then transpose
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    np_image = (np.transpose(np_image, (1, 2, 0)) - mean)/std    
+    np_image = np.transpose(np_image, (2, 0, 1))
+    
+    return np_image
+
+def predict(image_path, model, topk=5):
+    ''' Predict the class (or classes) of an image using a trained deep learning model.
+    '''
+    model.to(device)
+    
+    model.eval()
+    
+    # Turn the np_array image into a FloatTensor
+    # before running through model
+    tensor_img = torch.FloatTensor([process_image(image_path)])
+    # Note: model expects an 1D array of tensors of size (2, 244, 244)
+    # so that's why I'm putting the result of process_image()
+    # in brackets before passing into FloatTensor()
+    # in other words, model expects a tensor of size (1, 3, 244, 244)
+    # you coud also use `tensor_img = tensor_img.unsqueeze(0)`
+    
+    tensor_img = tensor_img.to(device)
+    
+    result = model(tensor_img).topk(topk)
+    if device == "cuda:0":
+        probs = torch.exp(result[0].data).cpu().numpy()[0]
+        idxs = result[1].data.cpu().numpy()[0]
+    else:
+        probs = torch.exp(result[0].data).numpy()[0]
+        idxs = result[1].data.numpy()[0]
+    
+    return(probs, idxs)
+
+test_image_path = args.image
+test_image_name = test_image_path.split('/')[-2]
+# Get the probabilties and indices from passing the image through the model
+probs, idxs = predict(test_image_path, loaded_model)
+
+if args.lables != None:
+    with open('cat_to_name.json', 'r') as f:
+        cat_to_name = json.load(f)
+    
+    test_image_name = cat_to_name[f"{test_image_name}"]
+    
+    # Swap the keys and values in class_to_idx so that
+    # indices can be mapped to original classes in dataset
+    idx_to_class = {v: k for k, v in loaded_model.class_to_idx.items()}
+
+    # Map the classes to flower category lables                              
+    idxs = list(map(lambda x: cat_to_name[f"{idx_to_class[x]}"], idxs))
+
+topk = zip(idxs, probs)
+
+print("Actual class:", test_image_name)
+
+print("\n---Top 5 predictions---")
+
+for i in topk:
+    print(f"{i[0]}: {i[1] * 100}%")
---
+++ final_project/train.py
@@ -0,0 +1,198 @@
+import torch
+import numpy as np
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+from collections import OrderedDict
+import argparse
+
+def build_network(archit="vgg16", out_features=102, hidden_layers=[1000]):
+    model = getattr(models, archit)(pretrained=True)
+
+    # Freeze the params from training
+    for param in model.parameters():
+        param.requires_grad = False\
+
+    # Find the number of in features the classifier expects
+    try:
+        iter(model.classifier)
+    except TypeError:
+        in_features = model.classifier.in_features
+    else:
+        in_features = model.classifier[0].in_features
+    
+    hidden_layers = [in_features] + hidden_layers
+    
+    # Define how to build a hidden layer
+    layer_builder = (
+        lambda i, v: (f"drop{i}", nn.Dropout()), 
+        lambda i, v: (f"fc{i}", nn.Linear(hidden_layers[i-1], v)),
+        lambda i, v: (f"relu{i}", nn.ReLU()),
+    )
+    
+    # Loop through hidden_layers array and build a layer for each
+    layers = [f(i, v) for i, v in enumerate(hidden_layers) if i > 0 for f in layer_builder]
+    # Concat the output layer onto the network
+    layers += [('fc_final', nn.Linear(hidden_layers[-1], out_features)),
+               ('output', nn.LogSoftmax(dim=1))]
+    
+    classifier = nn.Sequential(OrderedDict(layers))
+
+    # Replace classifier with our classifier
+    model.classifier = classifier
+
+    return model
+
+
+
+def validation(model, testloader, criterion):
+    accuracy = 0
+    test_loss = 0
+    model.eval() # Evaluation mode
+    with torch.no_grad():
+        for images, labels in testloader:
+            images, labels = images.to(device), labels.to(device)
+
+            output = model.forward(images)
+            test_loss += criterion(output, labels).item()
+
+            # Take exponential to get log softmax probibilities
+            ps = torch.exp(output)
+
+            # highest probability is the predicted class
+            # compare with true label
+            equality = (labels.data == ps.max(1)[1])
+
+            # Turn ByteTensor into np_array to calculate mean
+            accuracy += np.array(equality).mean()
+    
+    model.train() # Switch training mode back on
+    
+    return test_loss, accuracy
+
+def train(model, trainloader, testloader, criterion, optimizer, epochs=5, print_every=40):
+    
+    steps = 0
+    running_loss = 0
+    for e in range(epochs):
+        model.train() # Turn on training mode
+        
+        for images, labels in trainloader:
+            steps += 1
+            images, labels = images.to(device), labels.to(device)
+
+            optimizer.zero_grad() # Prevent gradients from accumulating
+            
+            output = model.forward(images)
+            loss = criterion(output, labels) # Calculate loss
+            # Back-prop
+            loss.backward()
+            optimizer.step()
+            
+            running_loss += loss.item()
+            if steps % print_every == 0:
+ 
+                model.eval()
+                
+                test_loss, accuracy = validation(model, testloader, criterion)
+                
+                print("Epoch: {}/{}.. ".format(e+1, epochs),
+                      "Training Loss: {:.3f}.. ".format(running_loss/print_every),
+                      "Test Loss: {:.3f}.. ".format(test_loss/len(testloader)),
+                      "Test Accuracy: {:.3f}".format(accuracy/len(testloader)))
+                
+                running_loss = 0
+                
+                model.train()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--arch', help="vgg or densenet", default='vgg')
+    parser.add_argument('-d', '--data_dir', default='flowers')
+    parser.add_argument('-hl', '--hidden_layers', default=[1000])
+    parser.add_argument('-e', '--epochs', default=3)
+    parser.add_argument('-g', '--gpu', default=True)
+    parser.add_argument('-lr', '--learning_rate', default=0.001)
+    args = parser.parse_args()
+
+    if args.arch == 'vgg':
+        arch = 'vgg16'
+    elif args.arch == 'densenet':
+        arch = 'densenet121'
+    else:
+        raise ValueError('Unexpected architecture', args.arch)
+
+    if args.gpu == True:
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    else:
+        device = torch.device("cpu")
+
+    data_dir = args.data_dir
+    train_dir = data_dir + '/train'
+    valid_dir = data_dir + '/valid'
+    test_dir = data_dir + '/test'
+
+    # Define normalization transform for reuse
+    noramlize = transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+
+    data_transforms = {
+        'train': transforms.Compose([transforms.RandomRotation(30),
+                                    transforms.RandomHorizontalFlip(),
+                                    transforms.RandomResizedCrop(224),
+                                    transforms.ToTensor(),
+                                    noramlize]),
+        'valid': transforms.Compose([transforms.Resize(255),
+                                    transforms.CenterCrop(224),
+                                    transforms.ToTensor(),
+                                    noramlize]),
+        'test': transforms.Compose([transforms.Resize(255),
+                                    transforms.CenterCrop(224),
+                                    transforms.ToTensor(),
+                                    noramlize])
+    }
+
+    # Load the datasets with ImageFolder
+    image_datasets = {
+        'train': datasets.ImageFolder(train_dir, transform=data_transforms['train']),
+        'valid': datasets.ImageFolder(valid_dir, transform=data_transforms['valid']),
+        'test':  datasets.ImageFolder(test_dir, transform=data_transforms['test'])
+    }
+
+    # define the dataloaders using the image datasets and the trainforms,
+    dataloaders = {
+        'train': torch.utils.data.DataLoader(image_datasets['train'], batch_size=64, shuffle=True),
+        'valid': torch.utils.data.DataLoader(image_datasets['valid'], batch_size=32, shuffle=True),
+        'test':  torch.utils.data.DataLoader(image_datasets['test'], batch_size=32, shuffle=True)
+    }
+
+    model = build_network(archit=arch,
+                          out_features=len(image_datasets['train'].class_to_idx),
+                          hidden_layers=args.hidden_layers)
+
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr=args.learning_rate)
+
+    print("begin training")
+    train(model, dataloaders['train'], dataloaders['valid'], criterion, optimizer, epochs=3, print_every=20)
+    print("trained\n\n")
+
+    # Check the test loss and accuracy of the trained network
+    test_loss, accuracy = validation(model, dataloaders['test'], criterion)
+    print("Network preformance on test dataset-------------")
+    print("Test Loss: {:.3f}.. ".format(test_loss/len(dataloaders['test'])),
+                          "Test Accuracy: {:.3f}".format(accuracy/len(dataloaders['test'])))
+
+    model.class_to_idx = image_datasets['train'].class_to_idx
+
+    checkpoint = {
+        "arch": "vgg16",
+        "class_to_idx": model.class_to_idx,
+        'state_dict': model.state_dict(),
+        "hidden_layers": [1000]
+    }
+
+    torch.save(checkpoint, "checkpoint.pt")
+    print("saved checkpoint")
+
+if __name__ == "__main__":
+    main()
