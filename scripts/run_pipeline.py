36a2c3484add2b40756b6e4acd79df94ec2c3156
dylanlrrb <dylan.r.larrabee@gmail.com>
2020-02-15 08:56:44 -0800

    Add come comments

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -12,3 +12,9 @@ tags = tags_to_build()
 # #   invoke the build scripts (accept tag name)
 # #   invole push script (accept tag name, s3 url)
 
+
+
+# Prefix a tag with "build_" then the version
+# suffix with "_<rebuild number>" to trigger a rebuild on a particular commit
+# "git push origin master --tags" to push the tags up to github to be pulled down later
+# git tag -l "build*" to list all tags that match the pattern
