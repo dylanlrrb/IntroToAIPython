8b1760f7429259ec10c109581a6d7bf79ff5d333
dylanlrrb <dylan.r.larrabee@gmail.com>
2020-02-17 20:27:23 -0800

    asdf

--- scripts/get_dataset.py
+++ scripts/get_dataset.py
@@ -9,12 +9,24 @@ def verify_and_download ():
       print(f'checking if required dataset "{DATASET_VERSION}/{UNCOMPRESSED_DATASET_NAME}" exists.')
       path_already_exists = path.isdir(f'{constants.WORKING_DIR}/datasets/{DATASET_VERSION}/{UNCOMPRESSED_DATASET_NAME}')
       if not path_already_exists:
-        print ('DATASET_URL, DATASET_VERSION', DATASET_URL, DATASET_VERSION)
         print(f'dataset "{DATASET_VERSION}/{UNCOMPRESSED_DATASET_NAME}" does not exist, downloading...')
-        call(['curl', '-o', 'temp.zip', DATASET_URL, '--create-dirs'])
-        call(['unzip', 'temp.zip', '-d', f'{constants.WORKING_DIR}/datasets/{DATASET_VERSION}/{UNCOMPRESSED_DATASET_NAME}'])
-        print('rm', 'temp.zip')
-        call(['rm', 'temp.zip'])
+        call([
+          'curl',
+          '-o',
+          f'{constants.WORKING_DIR}/datasets/{DATASET_VERSION}/{UNCOMPRESSED_DATASET_NAME}.zip',
+          DATASET_URL,
+          '--create-dirs'
+        ])
+        call([
+          'unzip',
+          f'{constants.WORKING_DIR}/datasets/{DATASET_VERSION}/{UNCOMPRESSED_DATASET_NAME}.zip',
+          '-d',
+          f'{constants.WORKING_DIR}/datasets/{DATASET_VERSION}/{UNCOMPRESSED_DATASET_NAME}'
+        ])
+        call([
+          'rm',
+          f'{constants.WORKING_DIR}/datasets/{DATASET_VERSION}/{UNCOMPRESSED_DATASET_NAME}.zip',
+          ])
       else:
         print(f'specified dataset "{DATASET_VERSION}/{UNCOMPRESSED_DATASET_NAME}" already exists.')
 
