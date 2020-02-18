521afbab3a558406496ede2b10674e7432c850e3
dylanlrrb <dylan.r.larrabee@gmail.com>
2020-02-17 17:47:47 -0800

    modify get script

--- scripts/get_dataset.py
+++ scripts/get_dataset.py
@@ -9,9 +9,13 @@ def verify_and_download ():
       print(f'checking if required dataset "{DATASET_DESTINATION}/" exists.')
       path_already_exists = path.isdir(f'{constants.WORKING_DIR}/{DATASET_DESTINATION}')
       if not path_already_exists:
+        print ('DATASET_URL, DATASET_DESTINATION', DATASET_URL, DATASET_DESTINATION)
         print(f'dataset "{DATASET_DESTINATION}/" does not exist, downloading...')
+        print('curl', '-o', f'{constants.WORKING_DIR}/{DATASET_DESTINATION}.zip', DATASET_URL)
         call(['curl', '-o', f'{constants.WORKING_DIR}/{DATASET_DESTINATION}.zip', DATASET_URL])
+        print('unzip', f'{constants.WORKING_DIR}/{DATASET_DESTINATION}.zip', '-d', constants.WORKING_DIR)
         call(['unzip', f'{constants.WORKING_DIR}/{DATASET_DESTINATION}.zip', '-d', constants.WORKING_DIR])
+        print('rm', f'{constants.WORKING_DIR}/{DATASET_DESTINATION}.zip')
         call(['rm', f'{constants.WORKING_DIR}/{DATASET_DESTINATION}.zip'])
       else:
         print(f'specified dataset "{DATASET_DESTINATION}/" already exists.')
